3785723fed587f2e0343189e7cd5122cd0dfbde0
guilospanck <guilherme.rp2010@hotmail.com>
2021-09-01 19:57:09 -0300

    feat: adding goroutine and binary_tree with channels

---
+++ exercise_equivalent_binary_trees/binary_trees.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+
+	Walk(t.Left, ch)
+	ch <- t.Value
+	Walk(t.Right, ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	// anonymous function
+	go func() {
+		defer close(ch1)
+		Walk(t1, ch1)
+	}()
+
+	go func() {
+		defer close(ch2)
+		Walk(t2, ch2)
+	}()
+
+	for {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+
+		if v1 != v2 || ok1 != ok2 {
+			return false
+		}
+
+		if !ok1 || !ok2 {
+			break
+		}
+
+	}
+
+	return true
+
+}
+
+func main() {
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+}
---
+++ goroutines.go
@@ -0,0 +1,30 @@
+/**
+* Concurrency X Parallelism
+*
+* - Concurrency: (Interruptability) => two lines of customers on the same cashier (lines taking turns ordering)
+* - Parallelism: (Independentability) => two lines of customers ordering from two cashiers (each line has its own cashier)
+*
+*
+*   See exercise_equivalent_binary_trees for an implementation of goroutines with channels
+*
+*  To use goroutine, just add the "go" syntax before a method or function
+*  Example:    go Fibonacci()
+*
+* => Channels are ways through which you can send and receive values from a routine.
+*       They can be created as:
+*       ch := make(chan int)
+*
+*       ch <- v    // Send v to channel ch.
+*       v := <-ch  // Receive from ch, and assign value to v.
+*
+*       (The data flows in the direction of the arrow.)
+*
+*       v, ok := <-ch
+*       // ok is false if there are no more values to receive and the channel is closed.
+*
+*       The loop
+*       for i := range ch
+*       receives values from the channel repeatedly until it is closed.
+*
+*
+**/
